821a3072fd3bc2d766a767a671f3ea6be8c025a3
Rodrigo Ribeiro <rodrigo.ribeiro03@hotmail.com>
2019-05-13 19:59:29 -0300

    Primeiro

---
+++ src/main/java/com/example/cobranca/receiver/Receiver.java
@@ -0,0 +1,15 @@
+package com.example.cobranca.receiver;
+
+import com.example.cobranca.model.Email;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+    @JmsListener(destination = "mailbox", containerFactory = "myFactory")
+    public void receiveMessage(Email email) {
+        System.out.println("Received <" + email + ">");
+    }
+
+}
